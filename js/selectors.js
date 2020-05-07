b66c384451f527d0d89329301a5ec0d55b0de64a
Mike Gretton <mikegretton@gmail.com>
2020-05-07 21:55:35 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,16 +1,16 @@
 // declare your functions here...
 function paragraphSelector() {
-
+  return $('p');
 }
 function lastImageSelector() {
-
+  return $('body img:last-child');
 }
 function ninjaBabySelector() {
-
+  return $('p');
 }
 function divSelector() {
-
+  return $('p');
 }
 function firstListItem() {
-
+  return $('p');
 }
